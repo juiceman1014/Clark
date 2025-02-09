58643537d4153e3d57bc603f83c74617f1058e11
juiceman1014 <justintran1014@gmail.com>
2025-02-08 17:20:36 -0800

    creating data function implemented

--- api/animal_api/routes/Animal.js
+++ api/animal_api/routes/Animal.js
@@ -7,3 +7,21 @@ const{
   NOT_FOUND
 } = require('../../util/constants').STATUS_CODES;
 
+router.post('/createAnimal', (req, res) => {
+  const { lifespan } = req.body;
+  const numberSent = !Number.isNaN(Number(lifespan));
+
+  const newEvent = new Animal({
+    name: req.body.name,
+    description: req.body.description,
+    lifespan: numberSent ? Number(lifespan) : undefined,
+  });
+
+  Animal.create(newEvent)
+    .then((post) => {
+      return res.json(post);
+    })
+    .catch(
+      (error) => res.sendStatus(BAD_REQUEST)
+    );
+});
