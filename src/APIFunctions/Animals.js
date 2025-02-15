eb9418b9d5b3848cb4069d83b17da870f2c48aa4
juiceman1014 <justintran1014@gmail.com>
2025-02-15 12:22:24 -0800

    admin can now create animals

--- src/APIFunctions/Animals.js
+++ src/APIFunctions/Animals.js
@@ -7,12 +7,27 @@ export async function getAllAnimals() {
   let status = new ApiResponse();
   await axios
     .get(ANIMAL_API_URL + '/Animal/getAnimals')
-    .then(res => {
+    .then((res) => {
       status.responseData = res.data;
     })
-    .catch(err => {
+    .catch((err) => {
       status.responseData = err;
       status.error = true;
+    });
+  return status;
+}
+
+export async function createAnimal(newAnimal, token) {
+  let status = new ApiResponse();
+  await axios
+    .post(ANIMAL_API_URL + '/Animal/createAnimal', newAnimal, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .catch((err) => {
+      status.error = true;
+      status.responseData = err;
     });
   return status;
 }
--- src/Pages/Animals/AdminAnimals.js
+++ src/Pages/Animals/AdminAnimals.js
@@ -1,8 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { getAllAnimals } from '../../APIFunctions/Animals';
+import React, { useEffect, useState } from 'react';
+import { getAllAnimals, createAnimal } from '../../APIFunctions/Animals';
 
-export default function AnimalPage() {
+export default function AnimalPage(props) {
   const [animals, setAnimals] = useState([]);
+  const [description, setDescription] = useState();
+  const [name, setName] = useState();
+  const [lifespan, setLifespan] = useState();
+
   async function getAnimalsFromDB() {
     const animalsFromDB = await getAllAnimals();
     if (!animalsFromDB.error) {
@@ -14,12 +18,74 @@ export default function AnimalPage() {
     getAnimalsFromDB();
   }, []);
 
+  const INPUT_CLASS = 'indent-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 text-white';
   return (
     <div className='m-10'>
-      <h1 className="text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+      <h1 class="text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
         Welcome to the Animal Admin Page!!
       </h1>
 
+      <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 grid-cols-full sm:grid-cols-6">
+        <div className="col-span-full sm:col-span-4">
+          <label htmlFor="name" className='block text-sm font-medium leading-6 text-gray-300'>
+            Animal Name
+          </label>
+          <div className="mt-2">
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="For example, Dog"
+              value={name}
+              onChange={e => setName(e.target.value)}
+              className={INPUT_CLASS}
+            />
+          </div>
+        </div>
+        <div className="col-span-full sm:col-span-4">
+          <label htmlFor="description" className='block text-sm font-medium leading-6 text-gray-300'>
+            Description
+          </label>
+          <div className="mt-2">
+            <input
+              type="text"
+              name="description"
+              id="description"
+              value={description}
+              onChange={e => setDescription(e.target.value)}
+              className={INPUT_CLASS}
+            />
+          </div>
+        </div>
+        <div className="col-span-full sm:col-span-4">
+          <label htmlFor="lifespan" className='block text-sm font-medium leading-6 text-gray-300'>
+            Lifespan
+          </label>
+          <div className="mt-2">
+            <input
+              type="text"
+              name="lifespan"
+              id="lifespan"
+              value={lifespan}
+              onChange={e => setLifespan(e.target.value)}
+              className={INPUT_CLASS}
+            />
+          </div>
+        </div>
+        <div className="col-span-full sm:col-span-4">
+          <button
+            type="submit"
+            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={() => createAnimal({
+              name,
+              description,
+              lifespan,
+            }, props.user.token)}
+          >
+            Save
+          </button>
+        </div>
+      </div>
 
       <div className="relative overflow-x-auto mt-10">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -30,6 +96,9 @@ export default function AnimalPage() {
               </th>
               <th scope="col" className="px-6 py-3">
                 Description
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Lifespan
               </th>
             </tr>
           </thead>
@@ -42,6 +111,9 @@ export default function AnimalPage() {
                   </th>
                   <td className="px-6 py-4">
                     {animal.description}
+                  </td>
+                  <td className="px-6 py-4">
+                    {animal.lifespan}
                   </td>
                 </tr>
               );
